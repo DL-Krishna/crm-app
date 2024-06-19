4b0a80db992379aff3a9687d999d17a0e33704ff
muralialakuntla3 <muralialakuntla3@gmail.com>
2024-06-19 23:35:31 +0530

    removed code which is not related to leads, learners, courses and home

--- backend/src/routes/messages.router.ts
+++
@@ -1,9 +0,0 @@
-import express from 'express';
-import { sendMessage, getAllMessages } from '../controllers/messages.controller';
-
-const router = express.Router();
-
-router.post('/send', sendMessage);
-router.get('/', getAllMessages);
-
-export default router;
